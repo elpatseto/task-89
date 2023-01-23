ebb5b23ed754ca935d5428618bcc1484d464211c
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-23 19:03:09 +0200

    Solve

--- src/js/Application.js
+++ src/js/Application.js
@@ -3,94 +3,91 @@ import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
 
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
-
-  constructor() {
-
-    super();
-
-    this._loading = document.querySelector("progress");
-    this._startLoading();
-    this._load();
-    this._stopLoading();
-    this._create();
-    this.emit(Application.events.READY);
-  }
-
-
-  async _startLoading(){
-    this._loading.style.visibility="visible";
-    return Promise.resolve("Loading starting");
-
-  }
-
-  async _stopLoading(){
-    const loadComplete = await this._load();
-    if(loadComplete === "Loading planets complete!") {
-      this._loading.style.visibility="hidden";
-    }else {
-      console.log("ERROR loading finish");
+    static get events() {
+        return {
+            READY: "ready",
+        };
     }
 
-  }
+    constructor() {
 
-  _load = async () => {
-    const loadComplete = await this._startLoading();
+        super();
 
-    if (loadComplete === "Loading starting") {
-      let url = "https://swapi.boom.dev/api/planets";
+        this._loading = document.querySelector("progress");
+        this._startLoading();
 
-      function checkStatus(responce) {
-        if (responce.status >= 200 && responce.status < 300) {
-          return Promise.resolve(responce);
-        } else {
-          return Promise.reject(new Error(responce.statusText));
+        this.emit(Application.events.READY);
+    }
+
+
+    _startLoading() {
+        this._loading.style.visibility = "visible";
+        this._load();
+    }
+
+    _stopLoading() {
+        this._loading.style.visibility = "hidden";
+    }
+
+    async _load() {
+
+        let responce = await fetch('https://swapi.boom.dev/api/planets');
+
+        let url = "https://swapi.boom.dev/api/planets";
+
+        function checkStatus(responce) {
+            if (responce.status >= 200 && responce.status < 300) {
+                return Promise.resolve(responce);
+            } else {
+                return Promise.reject(new Error(responce.statusText));
+            }
         }
-      }
 
-      function toJSON(responce) {
-        return responce.json();
-      }
+        function toJSON(responce) {
+            return responce.json();
+        }
 
 
-      fetch(url)
-          .then(checkStatus)
-          .then(toJSON)
-          .then((data) => {
-            data.results.forEach(d => {
-              this._create(d.name, d.terrain, d.population);
+        fetch(url)
+            .then(checkStatus)
+            .then(toJSON)
+            .then((data) => {
+                data.results.forEach(d => {
+                    this._create(d.name, d.terrain, d.population);
+                })
             })
-          })
+        for (let i = 2; i <= 6; i++) {
+            url = 'https://swapi.boom.dev/api/planets?page=' + 1;
+            fetch(url)
+                .then(checkStatus)
+                .then(toJSON)
+                .then((data) => {
+                    data.results.forEach(d => {
+                        this._create(d.name, d.terrain, d.population);
+                    })
+                })
+        }
+        this._stopLoading();
 
-      return Promise.resolve("Loading planets complete!");
 
-    }else {
-      console.log("ERROR");
+    }
+
+    _create(name, terrain, population) {
+        if (name !== undefined) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.innerHTML = this._render({
+                name: name,
+                terrain: terrain,
+                population: population,
+            });
+            document.body.querySelector(".main").appendChild(box);
+        }
     }
 
 
-  }
-
-  _create(name, terrain, population) {
-    if(name !== undefined) {
-      const box = document.createElement("div");
-      box.classList.add("box");
-      box.innerHTML = this._render({
-        name: name,
-        terrain: terrain,
-        population: population,
-      });
-      document.body.querySelector(".main").appendChild(box);
-    }
-  }
-
-
-  _render({name, terrain, population}) {
-    return `
+    _render({name, terrain, population}) {
+        return `
           <article class="media">
             <div class="media-left">
               <figure class="image is-64x64">
@@ -108,5 +105,5 @@ export default class Application extends EventEmitter {
             </div>
           </article>
     `;
-  }
+    }
 }
